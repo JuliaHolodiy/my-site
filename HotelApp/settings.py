12cd1427eaf21d4023de5344614507b5d0c68951
JuliaHolodiy <ulia.alekseevna@mail.ru>
2017-05-23 18:14:55 +0300

    Registration OK

--- HotelApp/settings.py
+++ HotelApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'registr',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -78,7 +79,7 @@ WSGI_APPLICATION = 'HotelApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db2.sqlite3'),
     }
 }
 
--- HotelApp/urls.py
+++ HotelApp/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registr.views import RegisterFormView
 
 urlpatterns = [
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^register/$', RegisterFormView.as_view()),
 ]
